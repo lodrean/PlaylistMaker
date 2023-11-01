8f8c15232b702c8119c860a56516292d2c468d08
Lodrean <lodrean@yandex.ru>
2023-11-01 19:11:50 +0300

    Домашнее задание спринт 8

--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -1,16 +1,14 @@
 package com.practicum.playlistmaker
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
         val backButton = findViewById<Button>(R.id.back)
-        backButton.setOnClickListener {startActivity(Intent(this, MainActivity::class.java))
-        }
+        backButton.setOnClickListener { super.finish() }
     }
 }
