26d549cc5ba6dba578950f2c1dc2d907ac45a6ad
Lodrean <lodrean@yandex.ru>
2023-11-14 20:10:27 +0300

    Домашнее задание спринт 9

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -1,11 +1,75 @@
 package com.practicum.playlistmaker
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 
 class SearchActivity : AppCompatActivity() {
+
+    var inputText: String = AMOUNT_DEF
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+        val clearButton = findViewById<ImageView>(R.id.clearIcon)
+        val inputEditText = findViewById<EditText>(R.id.inputEditText)
+        val backButton = findViewById<Button>(R.id.back)
+        inputEditText.setText(inputText)
+        backButton.setOnClickListener {
+            finish()
+        }
+        clearButton.setOnClickListener {
+            inputEditText.setText("")
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+            inputMethodManager?.hideSoftInputFromWindow(clearButton.windowToken, 0)
+        }
+
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // empty
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                clearButton.visibility = clearButtonVisibility(s)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                inputText.plus(s)
+            }
+        }
+        inputEditText.addTextChangedListener(simpleTextWatcher)
+    }
+
+
+    override fun onSaveInstanceState(outState: Bundle) {
+
+        super.onSaveInstanceState(outState)
+
+        outState.putString(TEXT_AMOUNT, inputText)
+
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        inputText = savedInstanceState.getString(TEXT_AMOUNT, AMOUNT_DEF)
+    }
+
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
+    }
+    companion object {
+        const val TEXT_AMOUNT = "TEXT_AMOUNT"
+        const val AMOUNT_DEF = ""
     }
 }
