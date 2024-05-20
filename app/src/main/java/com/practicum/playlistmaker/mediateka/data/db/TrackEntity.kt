162df5a04c5a94e6c517e59fa4f64c22ca966198
Lodrean <lodrean@yandex.ru>
2024-05-20 22:06:43 +0300

    21 домашня работа

---
+++ app/src/main/java/com/practicum/playlistmaker/mediateka/data/db/TrackEntity.kt
@@ -0,0 +1,20 @@
+package com.practicum.playlistmaker.mediateka.data.db
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "track_table")
+data class Track(
+    @PrimaryKey
+    val trackId: Int,
+    val trackName: String,
+    val artistName: String,
+    val url: String,
+    val trackTime: Int,
+    val artworkUrl100: String,
+    val collectionName: String,
+    val releaseDate: String,
+    val genre: String,
+    val country: String,
+)
