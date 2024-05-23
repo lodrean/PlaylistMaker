dddc26abf07bf2660b11e70a2e1571e8807b4e4a
lodrean <68153266+lodrean@users.noreply.github.com>
2024-05-23 22:52:11 +0300

    Update app/src/main/java/com/practicum/playlistmaker/mediateka/data/db/TrackEntity.kt
    Co-authored-by: AleksandrIlinskii <129396632+AleksandrIlinskii@users.noreply.github.com>

--- app/src/main/java/com/practicum/playlistmaker/mediateka/data/db/TrackEntity.kt
+++ app/src/main/java/com/practicum/playlistmaker/mediateka/data/db/TrackEntity.kt
@@ -3,7 +3,6 @@ package com.practicum.playlistmaker.mediateka.data.db
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-
 @Entity(tableName = "track_table")
 data class TrackEntity(
     @PrimaryKey val trackId: String,
