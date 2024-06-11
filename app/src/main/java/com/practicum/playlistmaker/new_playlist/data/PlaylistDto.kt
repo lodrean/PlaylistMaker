f3ec1ac92b09f5673db65b68a004c95d5e75bb34
Lodrean <lodrean@yandex.ru>
2024-06-11 22:51:19 +0300

    -

--- app/src/main/java/com/practicum/playlistmaker/new_playlist/data/PlaylistDto.kt
+++ app/src/main/java/com/practicum/playlistmaker/new_playlist/data/PlaylistDto.kt
@@ -1,4 +1,14 @@
 package com.practicum.playlistmaker.new_playlist.data
 
-class PlaylistDto {
-}
+import com.google.gson.annotations.SerializedName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class PlaylistDto(
+    @SerializedName("playlistId") val playlistId: String = "",
+    @SerializedName("playlistName") val playlistName: String = "",
+    @SerializedName("description") val description: String = "",
+    @SerializedName("imageUri") val imageUri: String = "",
+    @SerializedName("idList") val idList: List<String> = listOf(),
+    @SerializedName("tracksCount") val tracksCount: Int = 0
+)
