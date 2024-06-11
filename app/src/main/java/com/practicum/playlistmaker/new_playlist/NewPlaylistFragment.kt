00ac5f5d13c3ef5d87e909fc5bca68d9ec85db92
Lodrean <etabaev@gmail.com>
2024-06-11 08:03:46 +0300

    Домашнее задание 22 спринт

--- app/src/main/java/com/practicum/playlistmaker/new_playlist/NewPlaylistFragment.kt
+++ app/src/main/java/com/practicum/playlistmaker/new_playlist/NewPlaylistFragment.kt
@@ -1,13 +1,23 @@
 package com.practicum.playlistmaker.new_playlist
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.OnBackPressedCallback
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.net.toUri
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.practicum.playlistmaker.BindingFragment
 import com.practicum.playlistmaker.databinding.FragmentNewPlaylistBinding
+import java.io.File
+import java.io.FileOutputStream
 
 class NewPlaylistFragment : BindingFragment<FragmentNewPlaylistBinding>() {
 
@@ -45,11 +55,48 @@ class NewPlaylistFragment : BindingFragment<FragmentNewPlaylistBinding>() {
                 confirmDialog.show()
             }
         })
-
+        val pickMedia =
+            registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+                //обрабатываем событие выбора пользователем фотографии
+                if (uri != null) {
+                    binding.imageView.setImageURI(uri)
+                    saveImageToPrivateStorage(uri)
+                } else {
+                    Log.d("PhotoPicker", "No media selected")
+                }
+            }
+        //по нажатию на кнопку pickImage запускаем photo picker
+        binding.imageView.setOnClickListener {
+            pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+        }
+        //по нажатию на кнопку loadImageFromStorage пытаемся загрузить фотографию из нашего хранилища
+        /*binding.loadImageFromStorage.setOnClickListener {
+            val filePath = File(requireActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES), "myalbum")
+            val file = File(filePath, "first_cover.jpg")
+            binding.storageImage.setImageURI(file.toUri())
+        }*/
 
     }
 
     private fun save() {
 
+    }
+    private fun saveImageToPrivateStorage(uri: Uri) {
+        //создаём экземпляр класса File, который указывает на нужный каталог
+        val filePath = File(requireActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES), "myalbum")
+        //создаем каталог, если он не создан
+        if (!filePath.exists()){
+            filePath.mkdirs()
+        }
+        //создаём экземпляр класса File, который указывает на файл внутри каталога
+        val file = File(filePath, "first_cover.jpg")
+        // создаём входящий поток байтов из выбранной картинки
+        val inputStream = requireActivity().contentResolver.openInputStream(uri)
+        // создаём исходящий поток байтов в созданный выше файл
+        val outputStream = FileOutputStream(file)
+        // записываем картинку с помощью BitmapFactory
+        BitmapFactory
+            .decodeStream(inputStream)
+            .compress(Bitmap.CompressFormat.JPEG, 30, outputStream)
     }
 }
